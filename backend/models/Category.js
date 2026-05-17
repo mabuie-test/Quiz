5e5a17b22adc0b2c6a24c4890895e3727d81cfb5
Jorge Augusto Mabuie <jorgemaabuie@gmail.com>
2026-05-17 21:57:27 +0200

    Delete backend directory

--- backend/models/Category.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const CategorySchema = new mongoose.Schema({
-  name:        { type: String, required: true },
-  parentGroup: { type: String, enum: ['Ciências','Letras'], required: true },
-});
-module.exports = mongoose.model('Category', CategorySchema);
--- backend/models/Question.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const QuestionSchema = new mongoose.Schema({
-  text:        { type: String, required: true },
-  options:     [{ text: String, isCorrect: Boolean }],
-  explanation: { type: String, required: true },
-  category:    { type: mongoose.Schema.Types.ObjectId, ref: 'Category' },
-});
-module.exports = mongoose.model('Question', QuestionSchema);
